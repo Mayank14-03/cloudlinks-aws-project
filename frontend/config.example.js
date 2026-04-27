9f9ac6d10b4599d891d739a98dcf501281ea3158
Mayank Someshwar Korde <kordemayank1@gmail.com>
2026-04-27 15:09:25 +0530

    Update config.example.js

--- frontend/config.example.js
+++ frontend/config.example.js
@@ -1,8 +1,10 @@
 window.APP_CONFIG = {
-  API_BASE_URL: "https://replace-with-api-id.execute-api.ap-south-1.amazonaws.com",
-  COGNITO_DOMAIN: "https://replace-domain.auth.ap-south-1.amazoncognito.com",
-  COGNITO_CLIENT_ID: "replace-cognito-client-id",
-  COGNITO_REDIRECT_URI: "https://app.example.com/admin.html",
-  COGNITO_LOGOUT_URI: "https://app.example.com/admin-login.html",
+  API_BASE_URL: "https://cy7c9fapc9.execute-api.ap-south-1.amazonaws.com",
+  COGNITO_DOMAIN: "https://cloudlinks-766248020836-ap-south-1.auth.ap-south-1.amazoncognito.com",
+  COGNITO_CLIENT_ID: "3b1e2mqblsn87s7pbnlvltpidv",
+
+  COGNITO_REDIRECT_URI: "https://d19m7196xewb28.cloudfront.net",
+  COGNITO_LOGOUT_URI: "https://d19m7196xewb28.cloudfront.net",
+
   ADMIN_SESSION_IDLE_MINUTES: 30,
 };
